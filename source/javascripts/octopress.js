7fb97ffcdf2b6177f9b9be54359f6f69d175c497
ChaYoung You <yousbe@gmail.com>
2014-06-28 01:59:41 +0900

    Remove unused javascripts

--- source/javascripts/octopress.js
+++ source/javascripts/octopress.js
@@ -96,15 +96,6 @@ function wrapFlashVideos() {
   $('iframe[src*=vimeo],iframe[src*=youtube]').wrap('<div class="flash-video">')
 }
 
-function renderDeliciousLinks(items) {
-  var output = "<ul>";
-  for (var i=0,l=items.length; i<l; i++) {
-    output += '<li><a href="' + items[i].u + '" title="Tags: ' + (items[i].t == "" ? "" : items[i].t.join(', ')) + '">' + items[i].d + '</a></li>';
-  }
-  output += "</ul>";
-  $('#delicious').html(output);
-}
-
 $('document').ready(function() {
   testFeatures();
   wrapFlashVideos();
